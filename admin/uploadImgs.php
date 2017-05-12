89a48f88b2f909819b1c3164ec4ada435921c597
webDeveloper <webdesigner.mb@gmail.com>
2017-05-12 15:03:06 +0100

    Modal de final de upload / Alterações aos ajax
    Adicionado modal de final de upload;
    Alterações aos pedidos ajax para nova entrada para na BD e para enviar
    as duas imagens para as respectivas pastas

--- admin/uploadImgs.php
+++ admin/uploadImgs.php
@@ -1,6 +1,10 @@
 <?php
+	
+	var_dump("File: ");
+	var_dump( $_FILES );
 
-	//var_dump( $_FILES );
+	var_dump("Post: ");
+	var_dump($_POST);
 	
 	if ( 0 < $_FILES['file']['error'] ) {
         echo 'Error: ' . $_FILES['file']['error'] . '<br>';
